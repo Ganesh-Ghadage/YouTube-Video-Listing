b0319f64f1f3f3fd4717f25e362ccbf57710bb83
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-03-18 12:37:42 +0530

    Enhance YouTube video listing app with search functionality and improved layout

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-// const fetch = require('node-fetch');
-// import fetch  from 'node-fetch';
-
 async function fetchVideos(page = 1) {
-    const url = `https://api.freeapi.app/api/v1/public/youtube/videos?page=${page}`;
-    const options = {method: 'GET', headers: {accept: 'application/json'}};
-    
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
+  const url = `https://api.freeapi.app/api/v1/public/youtube/videos?page=${page}`;
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+  
+  try {
+    let responce = await fetch(url, options)
+    let data = await responce.json()
+    return data.data.data
+  } catch (error) {
+    console.log('Error while fetching data ', error)
+    return null
+  }
 }
 
-fetchVideos()
+let videoData;
+let page = 1;
+const videoGrid = document.querySelector('.videoGrid')
+
+
+async function fetchAndUseVideos(page) {
+  videoData = await fetchVideos(page);
+
+  videoData.forEach(video => {
+    const videoDiv = document.createElement('div')
+  
+    videoDiv.innerHTML = `
+      <div class="videoDiv">
+          <a href="https://www.youtube.com/watch?v=${video.items.id}" target="_blank">
+              <img id="videoThumbnail" src="${video.items.snippet.thumbnails.standard.url}" alt="${video.items.snippet.title} thumbnail">
+              <div class="videoInfo">
+                  <p id="videoTitle">${video.items.snippet.title}</p>
+                  <p id="videoChannel">${video.items.snippet.channelTitle}</p>
+              </div>
+          </a>
+      </div>`
+
+    videoGrid.appendChild(videoDiv)
+    
+  });
+    
+}
+
+fetchAndUseVideos(page++);
+
 
